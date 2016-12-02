889bb54780af9ab40490f5348cfa68672bd24d29
gc131 <gc2672@barnard.edu>
2016-12-01 20:39:16 -0500

    Add files via upload

--- gabbygame.js
+++ gabbygame.js
@@ -1,44 +1,52 @@
 console.log("game.js is working");
-//document.ready function
-//create the button listeners on the guess button, call the update function when clicked 
+$(document).ready(function(){
 
-alert ("The mystery number is between 1 and 100");
-var guess= prompt("Guess a number!");
-var guesss= parseInt(guess);
-var guesses= $(".movement").append(guesss)
+	$("#guessButton").click(update);
+})
 
-var turns = 5;
-
+var turns = 2;
 var answer = parseInt(Math.floor(Math.random()*100));
 
-//check function,
-//take in one parameter, the guess
-//if the guess is wrong, subract one from the turns
-//if the guess is right, display a win message
+var check =function(guesses){
 if (guesses == answer){
-  return "a win message"
+	$("body").css("background-image","url('success.gif')");
+	$("h1").css("color","black");
+	$("h2").css("color","black");
+	$("h3").css("color","black");
+	var sound = new Audio ("applause6.mp3");
+	sound.play();
+
+
+  return "You guessed the right number!"
 
 } else if (guesses > answer){
-	
-
+	turns = turns - 1;
 	return "Your guess was too high!";
   	
 } else if (guesses < answer){
-	 return "Your guess was too small!"
-}
-// } else {
-// 	alert("Thank you for playing!");
-// 	alert("The answer was" + answer);
-// }
+	turns= turns - 1;
+	 return "Your guess was too small!"	 
+} else {
+ 	return 
+}}
 
-//update function
-//called everytime the guess button is clicked
-//grab the users guess from the input with the class of "guess", that is the value in the box
-//parse int the value
-//call the check function with the user guess
-var msg = update(guess);
-//display the message of the click function to the screen
-//change the html inside the message box, class of msgBox, to be the msg
+var update= function(button){
+	if (turns > 0){
+		var guess  = parseInt($("#guessInput").val());
+		var msg = check(guess);
+		$(".msgBox").html(msg);
+
+	} else if (turns == 0){
+		var boo = new Audio ("boo3.mp3");
+		boo.play();
+
+		var msg = "Thank you for playing, you used up all your tries! the answer was" + answer;
+		$(".msgBox").html(msg);
+
+		 $("#guessButton").off();
+		
+
+}}
 
 
 
